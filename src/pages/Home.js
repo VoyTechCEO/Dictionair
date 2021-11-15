3f83b254793c000602c4225e6cb902197d3a531e
VoyTechCEO <makszokos@gmail.com>
2021-11-15 21:04:44 +0100

    added stats section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,6 +26,23 @@ const Home = () => {
           </div>
         </article>
       </div>
+      <div className='stats show-stats'>
+        <div className='content'>
+          <div className='stat'>
+            <h4>Zapamiętane słówka</h4>
+            <h4>173</h4>
+          </div>
+          <div className='stat'>
+            <h4>Problematyczne słówka</h4>
+            <h4>28</h4>
+          </div>
+          <div className='stat'>
+            <h4>Oczekujące słówka</h4>
+            <h4>119</h4>
+          </div>
+          <button>Wyczyść statystyki</button>
+        </div>
+      </div>
     </section>
   );
 };
