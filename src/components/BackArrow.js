659da09eab03a498c0f0dc0315d410d0596ee59a
VoyTechCEO <makszokos@gmail.com>
2021-12-04 14:04:57 +0100

    replace placeholders with svg

--- src/components/BackArrow.js
+++ src/components/BackArrow.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const BackArrow = () => {
-  return <h1>BackArrow</h1>;
+  return (
+    <a href='#test'>
+      <svg
+        width='45'
+        height='45'
+        version='1.1'
+        viewBox='0 0 71.66 52.917'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <g transform='translate(-34.374 -81.43)'>
+          <path
+            d='m61.689 134.35h-.86089l-26.454-26.458 26.454-26.458h1.7218v1.7177l-24.736 24.741 24.736 24.741v1.7177zm22.424-25.135h-20.678l-1.2428-1.3229 1.2428-1.3229h41.355l1.2428 1.3229-1.2428 1.3229z'
+            stroke-width='.26458'
+          />
+        </g>
+      </svg>
+    </a>
+  );
 };
 
 export default BackArrow;
